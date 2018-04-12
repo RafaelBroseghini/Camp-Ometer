f22cf9a5a1bd310cc42310ea6142c10aff09594f
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-04-12 11:45:39 -0500

    Changed let name

--- static/js/map.js
+++ static/js/map.js
@@ -16,6 +16,7 @@ let labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let labelIndex = 0;
 let map;
 let markers = [];
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'),{
         center: {
@@ -42,7 +43,7 @@ function plotMarkers(m) {
             animation: google.maps.Animation.DROP
         })
 
-        let content = '<div class="info-window">' + '<h3 id="park_map">' + marker[0] + ' National Park.</h3>' + '<div class="bodyContent">' + '<p style="color:black;font-size:12px;font-family:Montserrat">' + marker[3] + '</p>' + '<span id="coord" style="display:none;">{"lat": ' + marker[1] + ', "lon": ' + marker[2] + '}</span>' + '</div>' + '<div class="info-content">' + '<span> Web: ' + '<a href="' + marker[4] + '";font-size:10px;>' + marker[0] + '</a></span></p>' + '</div>' + '</div>'
+        let content = '<div class="info-window">' + '<h3 id="park_map">' + marker[0] + ' National Park </h3>' + '<div class="bodyContent">' + '<p style="color:black;font-size:12px;font-family:Montserrat">' + marker[3] + '</p>' + '<span id="coord" style="display:none;">{"lat": ' + marker[1] + ', "lon": ' + marker[2] + '}</span>' + '</div>' + '<div class="info-content">' + '<span> Web: ' + '<a href="' + marker[4] + '";font-size:10px;>' + marker[0] + '</a></span></p>' + '</div>' + '</div>'
 
         let infowindow = new google.maps.InfoWindow()
 
@@ -58,8 +59,8 @@ function plotMarkers(m) {
                 let doc = parser.parseFromString(infowindow.content, "application/xml");
                 let park_from_map = doc.querySelector("#park_map").textContent
                 setTitle(park_from_map)
-                let cont = JSON.parse(doc.getElementById("coord").textContent)
-                getForecast(cont);
+                let coord = JSON.parse(doc.getElementById("coord").textContent)
+                getForecast(coord);
             }
             ;
         }
